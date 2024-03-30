addc3ef379629fdd1b92957188799eff747b0aa5
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-03-30 12:01:35 +0700

    up algorithm

---
+++ algorithm/minimaxsum.js
@@ -0,0 +1,39 @@
+function miniMaxSum(arr) {
+    // Tính tổng toàn bộ mảng
+    const sum = arr.reduce((acc, val) => acc + val, 0);
+    // Khởi tạo min, max
+    let min = arr[0], max = arr[0];
+
+    // Tìm giá trị min và max
+    arr.forEach(val => {
+        if (val > max) max = val;
+        if (val < min) min = val;
+    });
+
+    // Im ra tổng nhỏ nhất và tổng lớn nhất
+    console.log((sum - max) + ' ' + (sum - min));
+
+}
+
+function bonusCalculations(arr) {
+    const sum = arr.reduce((acc, val) => acc + val, 0);
+    let min = arr[0], max = arr[0], evenCount = 0, oddCount = 0;
+
+    arr.forEach(val => {
+        if (val > max) max = val;
+        if (val < min) min = val;
+        if (val % 2 === 0) evenCount++;
+        else oddCount++;
+    })
+
+    console.log('Total of array:', sum);
+    console.log('Min in array:', min);
+    console.log('Max in array:', max);
+    console.log('Event elements count:', evenCount);
+    console.log('Odd elements count:', oddCount);
+}
+
+// Input
+const arr = [1, 2, 3, 4, 5];
+miniMaxSum(arr);
+bonusCalculations(arr);
